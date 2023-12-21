25e75e2a7a2b4e0a0937677b43d5141ed7f5b095
Andy Tran <andy.tran@tlgcohort.com>
2023-12-21 13:06:26 -0800

    styling

--- scripts/infoScript.js
+++ scripts/infoScript.js
@@ -1,6 +1,7 @@
 let url_string = window.location.href;
 let url1 = new URL(url_string);
 let key = url1.searchParams.get("key");
+let loc = url1.searchParams.get("loc");
 
 let url = "https://bypass-cors-jjg7.onrender.com";
 let res = fetch(url, {
@@ -36,7 +37,8 @@ function createWeatherCard({ weather }) {
   card.setAttribute("style", "width: 100");
   card.innerHTML = `
     <div class="card-body" style="display: flex; flex-direction: column; align-items: center;">
-      <h5 class="card-title">Weather for ${date}</h5>
+      <h2 class="card-title">Weather for ${loc}</h2>
+      <h5>${date}</h5>
       <h5 class="card-text">Summary: ${conditions}</h5>
       <p class="card-text">${text}</p>
       <a href="${link}" class="btn btn-primary">Learn more</a>
